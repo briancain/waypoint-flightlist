c32636e6a3090339404335788cbf58710d582833
Brian Cain <bcain@hashicorp.com>
2020-11-16 08:20:24 -0800

    Add scrap script for destroying existing waypoint server and volumes

---
+++ scripts/scrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function halt() {
+  # TODO: set warning with y/n prompt that this will
+  # volume prune
+  docker stop waypoint-server
+  docker rm waypoint-server
+  docker volume rm waypoint-server
+  docker volume prune -f
+}
+
+function demolish() {
+  halt
+  docker rmi hashicorp/waypoint
+}
