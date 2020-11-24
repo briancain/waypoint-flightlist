9c3c1cecad41fd298145cde2d9eef10fe48af126
Brian Cain <bcain@hashicorp.com>
2020-11-24 08:34:48 -0800

    Fix up spacing in script

--- k8s/setup-k8s.sh
+++ k8s/setup-k8s.sh
@@ -21,10 +21,11 @@ echo $"Obtaining container IP to set as range in metallb-config.yaml..."
 CONTAINERID=$(docker ps -a --filter="expose=6443" -q)
 IPADDR=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINERID)
 
-echo
 echo "IP Address of networked container is ${IPADDR}"
+echo
 read -p "Enter a range to define for metallb IP Addresses based on this container (like 172.18.0.20-172.18.0.50):`echo $'\n> '` " IPADDR_RANGE
 echo "IP Address range is ${IPADDR_RANGE}"
+echo
 
 sed s/%ADDR_RANGE%/$IPADDR_RANGE/g \
    metallb-config-template.yaml > metallb-config-set.yaml
@@ -32,5 +33,5 @@ sed s/%ADDR_RANGE%/$IPADDR_RANGE/g \
 echo "Applying metallb-config-set.yaml with ip address range applied..."
 kubectl apply -f metallb-config-set.yaml
 
-echo "Done! You should be ready to 'waypoint install' on kubernetes!"
+echo "Done! You should be ready to 'waypoint install -platform=kubernetes -accept-tos' on a local kubernetes!"
 exit 0
