b9dab9defb1b71669532f36387a167245c18cbfe
Brian Cain <bcain@hashicorp.com>
2020-11-24 08:26:31 -0800

    Add script for automating new k8s kind cluster with metallb

--- k8s/setup-k8s.sh
+++ k8s/setup-k8s.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Setting up kubernetes with kind and metallb...\n"
+
 echo "Creating kind cluster with cluster-config.yaml..."
 kind create cluster --config cluster-config.yaml
 
@@ -12,5 +14,19 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 echo "Applying metallb manifest..."
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 
-# docker inspect <container_id with ip>
-# grep and grab IPAddress and set inside metallb-config.yaml
+echo "\n\nObtaining container IP to set as range in metallb-config.yaml..."
+CONTAINERID=$(docker ps -a --filter="expose=6443" -q)
+IPADDR=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINERID)
+
+echo "IP Address of networked container is ${IPADDR}"
+read -p "Enter a range to define for metallb IP Addresses based on this container (like 172.18.0.20-172.18.0.50):`echo $'\n> '` " IPADDR_RANGE
+echo "IP Address range is ${IPADDR_RANGE}"
+
+sed s/%ADDR_RANGE%/$IPADDR_RANGE/g \
+   metallb-config-template.yaml > metallb-config-set.yaml
+
+echo "Applying metallb-config-set.yaml with ip address range applied..."
+kubectl apply -f metallb-config-set.yaml
+
+echo "Done! You should be ready to 'waypoint install' on kubernetes!"
+exit 0
