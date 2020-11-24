30ec5b738684e2ddad9dbee59ccd91267281cfdf
Brian Cain <bcain@hashicorp.com>
2020-11-24 07:58:13 -0800

    Add some output to script

--- k8s/setup-k8s.sh
+++ k8s/setup-k8s.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo "Creating kind cluster with cluster-config.yaml..."
 kind create cluster --config cluster-config.yaml
+
+echo "Applying metallb namespace..."
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+
+echo "Create secret for metallb-system node..."
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+echo "Applying metallb manifest..."
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 
 # docker inspect <container_id with ip>
