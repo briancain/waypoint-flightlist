e6bf0a92522ac4ab10da2b9e297765a474bede14
Brian Cain <bcain@hashicorp.com>
2020-12-04 08:26:22 -0800

    Add script for starting nomad in dev mode

---
+++ nomad/start-nomad.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+path_to_executable=$(which nomad)
+if ! [ -x "$path_to_executable" ] ; then
+  >&2 echo "error: nomad not defined on path"
+  exit 1
+fi
+
+NETWORK_INTERFACE=$1
+if ! [ -x "$NETWORK_INTERFACE" ] ; then
+  >&2 echo "error: no interface argument given"
+  echo "Usage:"
+  echo "start-nomad network_interface_name"
+  exit 1
+fi
+
+echo "Starting nomad agent in dev mode on ${NETWORK_INTERFACE}"
+nomad agent -dev -network-interface="${NETWORK_INTERFACE}"
+
+export NOMAD_ADDR='http://localhost:4646'
+
+echo
+echo "Finished!"
