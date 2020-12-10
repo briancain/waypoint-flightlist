12151275e3d51ff55ebd7cdd83c372ae3ca33597
Brian Cain <bcain@hashicorp.com>
2020-12-10 12:56:32 -0800

    Include WSL2 on setup since docker desktop seems to require it

--- windows/scripts/dockerdesktop.ps1
+++ windows/scripts/dockerdesktop.ps1
@@ -2,6 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 choco feature disable --name showDownloadProgress
 
-choco install vscode docker-desktop -y
+choco install docker-desktop -y --pre
 
 DISM /Online /Enable-Feature /All /NoRestart /FeatureName:Microsoft-Hyper-V
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
