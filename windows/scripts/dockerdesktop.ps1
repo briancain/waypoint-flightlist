f50ef10d8e2fd813b1edba154b6a1f14d43d1962
Brian Cain <bcain@hashicorp.com>
2020-12-21 08:36:30 -0800

    Remove pre from docker-desktop install

--- windows/scripts/dockerdesktop.ps1
+++ windows/scripts/dockerdesktop.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 choco feature disable --name showDownloadProgress
 
-choco install docker-desktop -y --pre
+choco install docker-desktop -y
 
 DISM /Online /Enable-Feature /All /NoRestart /FeatureName:Microsoft-Hyper-V
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
