7be8b9ab8e545001da1d4acd4939f1cefbaee69a
Brian Cain <bcain@hashicorp.com>
2021-02-04 14:20:12 -0800

    Ignore output from docker run

--- kind-k8s/setup-k8s.sh
+++ kind-k8s/setup-k8s.sh
@@ -10,7 +10,7 @@ running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || t
 if [ "${running}" != 'true' ]; then
   docker run \
     -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
-    registry:2
+    registry:2 | 2>/dev/null
 fi
 
 echo "Setting up kubernetes with kind and metallb..."
