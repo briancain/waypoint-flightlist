035455e9e508b205332bf313660fa2f7f355b508
Brian Cain <bcain@hashicorp.com>
2021-03-12 14:28:56 -0800

    Include running log command in benchmark test

--- scripts/endpoint-req.sh
+++ scripts/endpoint-req.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 while true; do
-  curl -s -o /dev/null -w "%{http_code}\n" $1 2>&1
+  curl -s -o /dev/null -w "response_code=%{http_code}\n" $1 2>&1
+  echo "executing 'waypoint logs'"
+  echo
+  waypoint logs > /dev/null 2>&1 &
+  export APP_PID=$!
   sleep 10
+  kill -INT $APP_PID
 done
