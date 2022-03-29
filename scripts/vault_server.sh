2a8b81365e7056d5e975d0e9347e29665e7e7501
Brian Cain <bcain@hashicorp.com>
2022-03-29 13:42:55 -0700

    Add some startup checks for vault server dev script

--- scripts/vault_server.sh
+++ scripts/vault_server.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-vault server -dev
+path_to_executable=$(which vault)
+if ! [ -x "$path_to_executable" ] ; then
+  >&2 echo "error: vault not defined on path"
+  exit 1
+fi
+
+NETWORK_INTERFACE=$1
+if [ -z "$NETWORK_INTERFACE" ] ; then
+  >&2 echo "error: no interface argument given"
+  echo "Usage:"
+  echo "vault_server network_interface_name"
+  exit 1
+fi
+
+echo "Starting vault server in dev mode on ${NETWORK_INTERFACE}"
+vault server -dev -dev-listen-address="${NETWORK_INTERFACE}" > vault.log 2>&1 &
